783817feff4de9beb02762d42d97f8ab954a1cfa
Fabio Filippelli <fabio.filippelli@googlemail.com>
2012-09-08 20:03:54 +0200

    Teständerung

--- HTWG-TaskManager/src/de/htwg/android/taskmanager/activity/MainActivity.java
+++ HTWG-TaskManager/src/de/htwg/android/taskmanager/activity/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends Activity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		// Änderung commiten!
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
