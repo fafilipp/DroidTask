3ec86e27cfe96addd15e29df430f677064ff8cd3
dagillet <dagillet@dagillet-PC>
2012-09-09 20:09:24 +0200

    blubb

--- HTWG-TaskManager/src/de/htwg/android/taskmanager/activity/MainActivity.java
+++ HTWG-TaskManager/src/de/htwg/android/taskmanager/activity/MainActivity.java
@@ -1,45 +1,86 @@
 package de.htwg.android.taskmanager.activity;
 
-import static de.htwg.android.taskmanager.util.constants.GoogleTaskConstants.GOOGLE_ACCOUNT_TYPE;
-import static de.htwg.android.taskmanager.util.constants.GoogleTaskConstants.LOG_TAG;
-import android.accounts.Account;
-import android.accounts.AccountManager;
+import de.htwg.android.taskmanager.backend.database.DatabaseHandler;
+import de.htwg.android.taskmanager.backend.entity.LocalTaskList;
+import de.htwg.android.taskmanager.google.sync.TaskData;
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.StrictMode;
-import android.util.Log;
 import android.view.Menu;
-
-import com.google.api.services.tasks.model.TaskList;
-
-import de.htwg.android.taskmanager.google.asynctasks.AsyncTasksManager;
-import de.htwg.android.taskmanager.google.sync.GoogleSyncException;
-import de.htwg.android.taskmanager.google.sync.IGoogleTaskManager;
 
 public class MainActivity extends Activity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		// Änderung commiten!
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-		StrictMode.setThreadPolicy(policy);
-		AccountManager accountManager = AccountManager.get(this);
-		Account[] accounts = accountManager.getAccountsByType(GOOGLE_ACCOUNT_TYPE);
-		Log.d(LOG_TAG, "amount of accounts = " + accounts.length);
-		if (accounts.length < 1) {
-			Log.i(LOG_TAG, "no google accounts found");
-		} else {
-			Log.d(LOG_TAG, "account name = " + accounts[0].name);
-			try {
-				IGoogleTaskManager taskManager = new AsyncTasksManager(this, accounts[0]);
-				for (TaskList taskList : taskManager.getTaskLists()) {
-					Log.i(LOG_TAG, taskList.getTitle());
-				}
-			} catch (GoogleSyncException e) {
-				e.printStackTrace();
-			}
+
+		// StrictMode.ThreadPolicy policy = new
+		// StrictMode.ThreadPolicy.Builder().permitAll().build();
+		// StrictMode.setThreadPolicy(policy);
+		// AccountManager accountManager = AccountManager.get(this);
+		// Account[] accounts =
+		// accountManager.getAccountsByType(GOOGLE_ACCOUNT_TYPE);
+		// Log.d(LOG_TAG, "amount of accounts = " + accounts.length);
+		// if (accounts.length < 1) {
+		// Log.i(LOG_TAG, "no google accounts found");
+		// } else {
+		// Log.d(LOG_TAG, "account name = " + accounts[0].name);
+		// try {
+		// IGoogleTaskManager taskManager = new AsyncTasksManager(this,
+		// accounts[0]);
+		// for (TaskList taskList : taskManager.getTaskLists()) {
+		// Log.i(LOG_TAG, taskList.getTitle());
+		// }
+		// } catch (GoogleSyncException e) {
+		// e.printStackTrace();
+		// }
+		// }
+
+		// try {
+		// DatabaseHandler db = new DatabaseHandler(this);
+		// TaskData td = new TaskData();
+		// Log.i(LOG_TAG, "Start saving data to the database");
+		// for (LocalTaskList tl : td.getAllTasklists().getListOfTaskList()) {
+		// db.addTaskList(tl);
+		// Log.i(LOG_TAG, tl.getTitle());
+		// }
+		// Log.i(LOG_TAG, "Data saved to the database");
+		//
+		// Log.i(LOG_TAG, "Retrieving data from the database");
+		// for (LocalTaskList tl : db.getAllTaskLists()) {
+		// Log.i(LOG_TAG, tl.getInternalId() + " - " + tl.getTitle());
+		// }
+		// Log.i(LOG_TAG, "Retrieving data from the database finished");
+		//
+		// Log.i(LOG_TAG, "Updating title of first tasklist");
+		// LocalTaskList ltl = db.getAllTaskLists().get(0);
+		// ltl.setTitle("New Tasklist title");
+		// db.updateTaskList(ltl);
+		// Log.i(LOG_TAG, "Updating title of first tasklist finished");
+		//
+		// Log.i(LOG_TAG,
+		// "Retrieving data from the database and deleting them");
+		// for (LocalTaskList tl : db.getAllTaskLists()) {
+		// Log.i(LOG_TAG, tl.getInternalId() + " - " + tl.getTitle());
+		// db.deleteTaskList(tl);
+		// }
+		// Log.i(LOG_TAG,
+		// "Retrieving data from the database and deleting them finished");
+		//
+		// Log.i(LOG_TAG, "Retrieving data from the database");
+		// for (LocalTaskList tl : db.getAllTaskLists()) {
+		// Log.i(LOG_TAG, tl.getInternalId() + " - " + tl.getTitle());
+		// }
+		// Log.i(LOG_TAG, "Retrieving data from the database finished");
+		//
+		// } catch (Exception e) {
+		//
+		// }
+
+		DatabaseHandler dbHandler = new DatabaseHandler(this);
+		TaskData taskData = new TaskData();
+		for(LocalTaskList ltl : taskData.getAllTasklists(dbHandler).getListOfTaskList()) {
+			System.out.println(ltl.getInternalId() + " - " + ltl.getTitle());
 		}
 	}
 
--- HTWG-TaskManager/src/de/htwg/android/taskmanager/google/sync/TaskData.java
+++ HTWG-TaskManager/src/de/htwg/android/taskmanager/google/sync/TaskData.java
@@ -2,39 +2,40 @@ package de.htwg.android.taskmanager.google.sync;
 
 import java.util.Date;
 
-import de.htwg.android.taskmanager.backend.binding.Binding;
-import de.htwg.android.taskmanager.backend.binding.MarshallingException;
+import de.htwg.android.taskmanager.backend.database.DatabaseHandler;
 import de.htwg.android.taskmanager.backend.entity.LocalTask;
 import de.htwg.android.taskmanager.backend.entity.LocalTaskList;
 import de.htwg.android.taskmanager.backend.entity.LocalTaskLists;
 
 public class TaskData {
 
-	private LocalTaskLists generateDummyData() throws MarshallingException {
-		LocalTaskLists tls = new LocalTaskLists();
-		int amountTaskLists = 3;
-		int amountTasks = 8;
-		for (int i = 0; i < amountTaskLists; i++) {
-			LocalTaskList tl = new LocalTaskList();
-			tl.setTitle("Tasklist " + i);
-			for (int j = 0; j < amountTasks; j++) {
-				LocalTask t = new LocalTask();
-				t.setTitle("Task " + i + " " + j);
-				t.setNotes("Hier werden die Notes angezeigt");
-				t.setDue(new Date().getTime() + 3600000);
-				tl.addTaskToList(t);
+	private void generateDummyIfNoData(DatabaseHandler dbHandler) {
+		if (dbHandler.getAllTaskLists().size() == 0) {
+			int amountTaskLists = 3;
+			int amountTasks = 8;
+			for (int i = 0; i < amountTaskLists; i++) {
+				LocalTaskList tl = new LocalTaskList();
+				tl.setTitle("Tasklist " + i);
+				dbHandler.addTaskList(tl);
+				for (int j = 0; j < amountTasks; j++) {
+					LocalTask t = new LocalTask();
+					t.setTitle("Task " + i + " " + j);
+					t.setNotes("Hier werden die Notes angezeigt");
+					t.setDue(new Date().getTime() + 3600000);
+					tl.addTaskToList(t);
+					dbHandler.addTask(tl, t);
+				}
 			}
-			tls.addTaskList(tl);
 		}
-		Binding binding = new Binding();
-		binding.marshall(tls);
-		return tls;
 	}
 
-	public LocalTaskLists getAllTasklists() throws MarshallingException {
-		generateDummyData();
-		Binding binding = new Binding();
-		return binding.unmarshall();
+	public LocalTaskLists getAllTasklists(DatabaseHandler dbHandler) {
+		generateDummyIfNoData(dbHandler);
+		LocalTaskLists tls = new LocalTaskLists();
+		for (LocalTaskList ltaskList : dbHandler.getAllTaskLists()) {
+			tls.addTaskList(ltaskList);
+		}
+		return tls;
 	}
 
 }
