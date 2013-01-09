956dbcf8af0e2337ed265f557edf558f2501927d
dagillet <dagillet@Dell-Vostro3450>
2013-01-09 22:27:32 +0100

    test

--- HTWG-TaskManagerTest/src/de/htwg/android/taskmanager/activity/MainActivityTest.java
+++ HTWG-TaskManagerTest/src/de/htwg/android/taskmanager/activity/MainActivityTest.java
@@ -1,8 +1,10 @@
 package de.htwg.android.taskmanager.activity;
 
+import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
-import android.view.View;
-import android.widget.EditText;
+import de.htwg.android.taskmanager.backend.database.DatabaseHandler;
+import de.htwg.android.taskmanager.backend.entity.LocalTask;
+import de.htwg.android.taskmanager.backend.entity.LocalTaskList;
 
 public class MainActivityTest extends
 		ActivityInstrumentationTestCase2<MainActivity> {
@@ -13,25 +15,39 @@ public class MainActivityTest extends
 
 	public void testActivityTestCaseSetUpProperly() {
 		assertNotNull("activity should be launched successfully", getActivity());
+		
+		
+		
 	}
 	
-	public void testActivityPause(){
+	public void testTaskActivity(){
+		DatabaseHandler db = new DatabaseHandler(getActivity());
+		LocalTaskList localTaskList = new LocalTaskList();
+		localTaskList.setTitle("TestTaskList");
+		LocalTask localTask = new LocalTask();
+		localTask.setTitle("TestTask");
+		db.addTaskList(localTaskList);
+		db.addTask(localTaskList, localTask);
 		
-		final View view = getActivity().findViewById(R.id.add);
-		
-		getActivity().runOnUiThread(new Runnable() {
-			
-			@Override
-			public void run() {
-				view.requestFocus();
-				view.callOnClick();
-				
-//				EditText title = (EditText) getActivity().findViewById(R.id.title);
-				
-			}
-		});
-		
-//		getActivity().finish();
-		
+		Intent intent = new Intent(getActivity(), TaskActivity.class);
+		intent.putExtra("task-id", localTask.getInternalId());
+		setActivityIntent(intent);
 	}
+	
+	public void testNewAndEditTaskActivity(){
+		
+		DatabaseHandler db = new DatabaseHandler(getActivity());
+		LocalTaskList localTaskList = new LocalTaskList();
+		localTaskList.setTitle("TestTaskList");
+		LocalTask localTask = new LocalTask();
+		localTask.setTitle("TestTask");
+		db.addTaskList(localTaskList);
+		db.addTask(localTaskList, localTask);
+		
+		Intent intent = new Intent(getActivity(), NewAndEditTaskActivity.class);
+		intent.putExtra("task-id", localTask.getInternalId());
+		setActivityIntent(intent);
+	}
+	
+
 }
